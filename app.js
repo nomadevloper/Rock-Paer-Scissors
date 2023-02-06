52cd3ad8e84efe14b4e9f2e416bb6274e7246217
nomadevloper <122037674+nomadevloper@users.noreply.github.com>
2023-02-06 21:22:03 +0900

    Add buttons instead of using console

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
+const buttons = document.querySelectorAll("button");
+const divResult = document.querySelector(".result");
+const scoreboard = document.querySelector(".scoreboard");
+const resetBtn = document.querySelector(".reset");
+const mapping = {
+    rock: 0,
+    paper: 1,
+    scissors: 2
+}
+
+let playerWinCount = 0;
+let computerWinCount = 0;
+let result = '';
+
 function getComputerChoice() {
     const options = ["rock", "paper", "scissors"];
     return options[Math.floor(Math.random() * options.length)];
@@ -7,65 +21,66 @@ function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-const mapping = {
-    rock: 0,
-    paper: 1,
-    scissors: 2
-}
-
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
     computerSelection = computerSelection.toLowerCase();
-    playerMapping = mapping[playerSelection];
-    computerMapping = mapping[computerSelection];
+    const playerMapping = mapping[playerSelection];
+    const computerMapping = mapping[computerSelection];
 
-    const isPlayerWin = playerMapping - computerMapping === 1 || playerMapping - computerMapping === -2;
-    const isComputerWin = computerMapping - playerMapping === 1 || computerMapping - playerMapping === -2;
+    const playerWin = playerMapping - computerMapping === 1 || playerMapping - computerMapping === -2;
+    const computerWin = computerMapping - playerMapping === 1 || computerMapping - playerMapping === -2;
 
-    if (isPlayerWin) {
-        return {
-            winner: "player",
-            msg: `You Win! ${capitalizeFirstLetter(playerSelection)} beats ${capitalizeFirstLetter(computerSelection)}`
-        };
-    } else if (isComputerWin) {
-        return {
-            winner: "computer",
-            msg: `You Lose! ${capitalizeFirstLetter(computerSelection)} beats ${capitalizeFirstLetter(playerSelection)}`
-        };
+    if (playerWin) {
+        playerWinCount++;
+        return `You Win! ${capitalizeFirstLetter(playerSelection)} beats ${capitalizeFirstLetter(computerSelection)}`;
+    } else if (computerWin) {
+        computerWinCount++;
+        return `You Lose! ${capitalizeFirstLetter(computerSelection)} beats ${capitalizeFirstLetter(playerSelection)}`;
     } else {
-        return {
-            msg: "You Draw!"
-        };
+        return "You Draw!";
+    }
+}
+
+function updateScore() {
+    divResult.textContent = result;
+    scoreboard.textContent = `${playerWinCount} : ${computerWinCount}`;
+}
+
+function isGameOver() {
+    return playerWinCount === 5 || computerWinCount === 5;
+}
+
+function restartGame() {
+    playerWinCount = 0;
+    computerWinCount = 0;
+    result = '';
+    updateScore();
+}
+
+resetBtn.addEventListener("click", function(){
+    restartGame();
+});
+
+function clickButton() {
+    const playerSelection = this.textContent;
+    const computerSelection = getComputerChoice();
+
+    result = playRound(playerSelection, computerSelection);
+    updateScore();
+    console.log(`${playerWinCount} : ${computerWinCount}`);
+    if (isGameOver()) {
+        const winner = playerWinCount > computerWinCount ? "Player" : "Computer";
+        const wantToRestart = confirm(`Winner : ${winner}\nPress comfirm button to restart the game`);
+        if (wantToRestart) {
+            restartGame();
+        }
     }
 }
 
 function game() {
-    let playerWin = 0;
-    let computerWin = 0;
-    for (let i = 0; i < 5; i++) {
-        let playerSelection;
-        do {
-            playerSelection = prompt("Choose between Rock, Paper and Scissors");
-        } while(!(playerSelection in mapping))
-        const computerSelection = getComputerChoice();
-        result = playRound(playerSelection, computerSelection);
-        console.log(result.msg);
-        if (result.winner) {
-            if (result.winner === "player") {
-                playerWin ++;
-            } else {
-                computerWin ++;
-            }
-        }
-    }
-    console.log(`You ${playerWin} : Com ${computerWin}`);
-    if (playerWin > computerWin) {
-        console.log("You Win the Game!");
-    } else if (playerWin < computerWin) {
-        console.log("You Loose the Game!");
-    } else {
-        console.log("You Draw the Game!");
-    }
+    scoreboard.textContent = `${playerWinCount} : ${computerWinCount}`;
+    buttons.forEach(button => button.addEventListener("click", clickButton));
+    resetBtn.removeEventListener("click", clickButton);
 }
 
 game();
